3d0fbd6deddddb39699f6a0faa63823b92085cd5
AixitaMartinez <maraixasociales@gmail.com>
2023-10-07 22:24:08 -0500

    lo ultimoooooooooooooooooo

--- crud/administrador/actualizar-examen.php
+++ crud/administrador/actualizar-examen.php
@@ -1,16 +1,7 @@
 <?php
-session_start();
-if (!isset($_SESSION['docentes'])) {
-    echo '
-    <script>
-    alert("Por favor, debes iniciar sesión");
-    window.location = "../index.php";
-    </script>';
-    session_destroy();
-    exit;
-}
 include '../php/conexion_be.php';
 
+// Consultar id del examen
 $id = $_GET['id'];
 $id = filter_var($id, FILTER_VALIDATE_INT);
 
@@ -19,7 +10,7 @@ if (!$id) {
     header('location: editar-examen.php');
     exit;
 }
-var_dump($id);
+
 $query = "SELECT * FROM examenes WHERE id = $id";
 $resultado = mysqli_query($conexion, $query);
 $examen = mysqli_fetch_assoc($resultado);
@@ -27,13 +18,16 @@ $examen = mysqli_fetch_assoc($resultado);
 // Verificar si el formulario se ha enviado
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nombre = $_POST['nombre']; // Nombre del examen
- 
+    // Aquí debes obtener y actualizar otros campos si es necesario
+
+    // Actualizar los datos del examen en la base de datos
     $query = "UPDATE examenes SET nombre = '$nombre' WHERE id = $id";
     $resultado = mysqli_query($conexion, $query);
 
     if ($resultado) {
         echo "Examen actualizado correctamente.";
-   
+        // Puedes redirigir al usuario a una página de confirmación o a la lista de exámenes
+        // header('location: lista-examenes.php');
     } else {
         echo "Error al actualizar el examen.";
     }
@@ -52,9 +46,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <form method="POST" action="">
         <label for="nombre">Nombre del Examen:</label>
         <input type="text" name="nombre" id="nombre" value="<?php echo $examen['nombre']; ?>">
-        <!-- no sé que hice con lo demás xd-->
+        <!-- Aquí puedes agregar campos adicionales para editar otros datos del examen -->
         
- 
+        <!-- Puedes agregar más campos para editar otros datos del examen -->
 
         <input type="submit" value="Guardar Cambios">
     </form>
